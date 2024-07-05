a3e26e6712e0fcee23136f6a04111921a2bd4500
shakewen <2670305874@qq.com>
2024-07-05 21:52:29 +0800

    Updating

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
@@ -10,13 +11,23 @@ public class ItemController : MonoBehaviour
 
    [SerializeField] private float outlineOffset = 3f;
 
-   [SerializeField] private bool startOutline = false;
+   //public bool isInteract = false;
 
     private List<SpriteRenderer> ItemSpriteRenderer;
 
+    //设置每个可采样的物体是否进入交互状态
+    public Dictionary<SpriteRenderer, bool> interactDictionary = new Dictionary<SpriteRenderer, bool>();
+
+
     public SpriteRenderer currentSpriteRenderer;
 
+    public SpriteRenderer CurrentInteractSR;
+
+    public MaterialPropertyBlock block;
+
     
+
+    [SerializeField] private float fadeSpeed = 3.5f;
 
 
      void OnEnable()
@@ -28,12 +39,23 @@ public class ItemController : MonoBehaviour
     void Start()
     {
         ItemSpriteRenderer = new List<SpriteRenderer>();
-        itemController=GameObject.Find("ItemController");
+
+        itemController=GameObject.FindWithTag("ItemController");
+
+        block = new MaterialPropertyBlock();
+
+
+
         for (int i = 0; i < itemController.transform.childCount; i++)
         {
-            ItemSpriteRenderer.Add(itemController.transform.GetChild(i).GetComponent<SpriteRenderer>());
+           SpriteRenderer itemRenderer = itemController.transform.GetChild(i).GetComponent<SpriteRenderer>();
+           
+            ItemSpriteRenderer.Add(itemRenderer);
+
+            //设置键值对应初始为false
+            interactDictionary[itemRenderer] = false;
         }
-       
+
         
     }
 
@@ -45,35 +67,46 @@ public class ItemController : MonoBehaviour
 
     void Update()
     {
-        if (currentSpriteRenderer == null)
-            return;
-            
-            //当前物体，是否开启轮廓，轮廓颜色，轮廓采样偏移量（锐化程度）
-            UpdateOutline(currentSpriteRenderer, startOutline, color,outlineOffset);
-        
+
+        //是否开启轮廓，当前物体，轮廓颜色，轮廓采样偏移量（锐化程度）
+        UpdateOutline(interactDictionary[currentSpriteRenderer], outlineOffset);
+
+
+        IsInteract();
     }
 
     //是否开启轮廓，轮廓渐变
-    void UpdateOutline(SpriteRenderer itemRenderer,bool gradualchangeColor,Color _color,float outlineOffset)
+    void UpdateOutline(bool gradualchangeColor, float outlineOffset)
     {
-        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        if (currentSpriteRenderer == null)
+            return;
 
-        itemRenderer.GetPropertyBlock(block);
+        currentSpriteRenderer.GetPropertyBlock(block);
 
         block.SetFloat("_Outline", gradualchangeColor ? 1f : 0);
         //使用全局变量的字符串
         block.SetFloat("_SampleOffset", outlineOffset);
 
-        //设置渐变效果要设置为全局变量
-        block.SetColor("_OutlineColor", _color);
+        //color.a =Mathf.Clamp(color.a,0,1);
 
-        
-        itemRenderer.SetPropertyBlock(block);
+        //////暂时取消渐变，因为物体密集度太高
+        //if (gradualchangeColor)
+        //{
+        //    color.a += Time.deltaTime * fadeSpeed;
+        //}
+        //else
+        //{
+        //    color.a -= Time.deltaTime * fadeSpeed;
+        //}
+        //设置渐变效果要设置为全局变量
+        block.SetColor("_OutlineColor", color);
+
+        currentSpriteRenderer.SetPropertyBlock(block);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
+        
         OnPlayerEnterItem(collision,true);
 
       
@@ -86,18 +119,61 @@ public class ItemController : MonoBehaviour
     }
 
    
-    void OnPlayerEnterItem(Collider2D collision,bool _isOutline)
+    void OnPlayerEnterItem(Collider2D collision,bool _isInteract)
     {
-        if (collision.CompareTag("Item"))
+        foreach (SpriteRenderer itemRenderer in ItemSpriteRenderer)
         {
-            foreach (SpriteRenderer itemRenderer in ItemSpriteRenderer)
-            {
-                if (itemRenderer.gameObject == collision.gameObject)
-                {
-                    currentSpriteRenderer = itemRenderer;
 
-                    startOutline = _isOutline;
+            if (itemRenderer.gameObject == collision.gameObject)
+            {
+                
+                if (collision.gameObject.layer==(LayerMask.NameToLayer("Transparency")))
+                {
+                   
+                    isTransparecy(itemRenderer, _isInteract);
                 }
+
+                //主角触碰到可交互物体
+                if (collision.CompareTag("Item"))
+                {
+                    print(itemRenderer.name);
+
+                        //当前可被采样的物体
+                        currentSpriteRenderer = itemRenderer;
+
+                    //只接收当前在玩家进入时的物体才能被交互
+                    if (_isInteract)
+                    {
+                        //当前可交互物体
+                        CurrentInteractSR = itemRenderer;
+
+                    }
+                        
+                    interactDictionary[itemRenderer] = _isInteract;
+                }
+
+            }
+
+
+        }
+
+    }
+
+    void isTransparecy(SpriteRenderer _itemRenderer,bool _isTransparent)
+    {
+        _itemRenderer.color = new Color(_itemRenderer.color.r, _itemRenderer.color.g, _itemRenderer.color.b, _isTransparent ? 0.65f : 1);
+    }
+
+    void IsInteract()
+    {
+        //当前可交互的物体能够交互
+        if (interactDictionary[CurrentInteractSR])
+        {
+            if (InputManager.GetJInput())
+            {
+                print("可以交互");
+                //显示文本框输出文本
+                //等待文本全部输入完成，再次点击鼠标左键，关闭文本框
             }
         }
     }
